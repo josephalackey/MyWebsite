6844911e1e3ea98d44fe319660c50d813af18202
josephalackey <57867211+josephalackey@users.noreply.github.com>
2020-01-04 16:14:38 -0500

    First
    Testing out commiting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import selfPortrait from "./images/portrait.jpg";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+var bodyStyle = {
+  backgroundImage: `url(${selfPortrait})`,
+  backgroundSize: "200vh",
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  minHeight: "75vh"
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="websiteBody" >
+      <p style={bodyStyle}>
+        <div className="menuBar">
+          <a href=""> Home </a>
+          <a href=""> Experience </a>
+          <a href=""> Blog </a>
+          <a href=""> About Me </a>
+        </ div>
+        <h1 className="portraitHeader"> Joseph Lackey <br /> Project Manager - Web Developer </ h1>
+      </p>
     </div>
-  );
+  }
 }
 
 export default App;
